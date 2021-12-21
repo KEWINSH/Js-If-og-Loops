627da550a463fa4b770473f68345bb606ac8b91b
KEWINSH <93644595+KEWINSH@users.noreply.github.com>
2021-12-22 00:12:54 +0100

    Add files via upload

---
+++ Js Oppgave/Js if og loops .js
@@ -0,0 +1,72 @@
+// Oppgave 1
+/*
+
+var s = window.prompt('Enter your input: (i.e: 24C)')
+var r = ""
+
+if (s[-1] == 'c'  'C') {
+    r = parseInt(s)
+    r = (r * 9/5) + 32
+    document.write(r + 'f')
+}else {
+    r = parseInt(s)
+    r = (r - 32) * 5/9
+    document.write(r + 'c')
+}
+/
+// Oppgave 2
+/
+var s = window.prompt('Enter your number:')
+var r = 0
+
+
+if (s > 10) {
+    r = (s-10) * 2
+}else {
+    r = 10 - s
+}
+
+/
+// Oppgave 3 
+
+/
+var s = parseInt(window.prompt('Enter your number:'))
+var s2 = parseInt(window.prompt('Enter your second number:'))
+var r = 0
+
+if (s == s2) {
+    document.write((s+s2)*3)
+}else {
+    document.write(s + s2)
+}
+/
+
+// Oppgave 4
+
+/
+var s = parseInt(window.prompt('Enter your number:'))
+var s2 = parseInt(window.prompt('Enter your second number:'))
+
+if (s == 50  s2 == 50 || s2 + s == 50) {
+    document.write('true')
+}else {
+    document.write('false')
+}
+/
+
+// Oppgave 5
+var r = ''
+var p = ""
+for (var i = 0; i < 10; i++) {
+    if (i > 5) {
+        p = p.replace(r, '')
+    }else {
+        p += r
+    }
+    document.write("<p>"+p+"</p>")
+
+}
+
+// Oppgave 6
+
+*/
